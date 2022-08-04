2d288bf16691af0805bb85c9a14ebd96f450cedd
huy.doan <huy.doan@gitlab.id>
2022-08-04 17:30:02 +0700

    first commit

---
+++ src/main/java/com/huy/SqServlet.java
@@ -0,0 +1,17 @@
+package com.huy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	public void doGet(HttpServletRequest rq, HttpServletResponse rs) throws IOException{
+		int k = (int) rq.getAttribute("k");
+		PrintWriter out = rs.getWriter();
+		k = k * k;
+		out.print("Result is: " + k);
+	}
+}
