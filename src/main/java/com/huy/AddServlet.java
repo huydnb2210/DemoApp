d54dafbb608b9d41bdee2e3e214d124dac6274b5
huy.doan <huy.doan@gitlab.id>
2022-08-05 11:00:51 +0700

    Update AddServlet and SqServlet file

--- src/main/java/com/huy/AddServlet.java
+++ src/main/java/com/huy/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
@@ -15,11 +16,11 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
-		
-		req.setAttribute("k", k);
-		
-		RequestDispatcher rd = req.getRequestDispatcher("sq");
-		rd.forward(req, res);
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		res.sendRedirect("sq?k=" +k); // URL rewriting
+//		RequestDispatcher rd = req.getRequestDispatcher("sq");
+//		rd.forward(req, res);
 	
 	}
 
--- src/main/java/com/huy/SqServlet.java
+++ src/main/java/com/huy/SqServlet.java
@@ -6,12 +6,17 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest rq, HttpServletResponse rs) throws IOException{
-		int k = (int) rq.getAttribute("k");
+		HttpSession session = rq.getSession();
+		
+		int k = (int) session.getAttribute("k");
 		PrintWriter out = rs.getWriter();
 		k = k * k;
 		out.print("Result is: " + k);
+		
+		System.out.println("sq called");
 	}
 }
