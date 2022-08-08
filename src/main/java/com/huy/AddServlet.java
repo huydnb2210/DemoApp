a5c7f0d874fe0a5d3aedec550fcab01141077d45
huy.doan <huy.doan@gitlab.id>
2022-08-08 15:45:59 +0700

    Second edit

--- src/main/java/com/huy/AddServlet.java
+++ src/main/java/com/huy/AddServlet.java
@@ -5,23 +5,41 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
-		res.sendRedirect("sq?k=" +k); // URL rewriting
+//		Cookie cookie = new Cookie("k", k + "");
+//		res.addCookie(cookie); // Cookie
+		
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+	    
+//		res.sendRedirect("sq?k=" +k); // URL rewriting
+
+			
 //		RequestDispatcher rd = req.getRequestDispatcher("sq");
 //		rd.forward(req, res);
+		
+		PrintWriter out = res.getWriter();
+		out.println("<html><body bgcolor ='red'>");
+		out.println("Output: " + k);
+		out.println("</body></html>");
 	
 	}
+	
 
 }
+
--- src/main/java/com/huy/SqServlet.java
+++ src/main/java/com/huy/SqServlet.java
@@ -3,18 +3,32 @@ package com.huy;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/sq")
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest rq, HttpServletResponse rs) throws IOException{
-		HttpSession session = rq.getSession();
+		int k = 0;
+		Cookie cookies[] = rq.getCookies();
 		
-		int k = (int) session.getAttribute("k");
-		PrintWriter out = rs.getWriter();
+		for (Cookie c : cookies) {
+			if (c.getName().equals("k")) {
+				k = Integer.parseInt(c.getValue());
+			}
+		}
+		
+//		HttpSession session = rq.getSession(); //HttpSession
+//		
+//		int k = (int) session.getAttribute("k");
 		k = k * k;
+		PrintWriter out = rs.getWriter();
+		
 		out.print("Result is: " + k);
 		
 		System.out.println("sq called");
